ce38671286cd705e672e3612f1842ae454b4942f
Md. Imrul Hasan <imrulhasan273@gmail.com>
2020-12-11 19:37:39 +0600

    a

---
+++ codes/notes.sql
@@ -0,0 +1,139 @@
+
+--------------------------------------CREATION PART-----------------------------
+---CREATING A TABLE
+CREATE TABLE ict_2020.links (
+   link_id serial PRIMARY KEY,
+   title VARCHAR (512) NOT NULL,
+   url VARCHAR (1024) NOT null 
+);
+
+
+
+---select operation
+select * from ict_2020.links l ;
+
+
+--drop table			(DATA WILL LOSS)
+drop table ict_2020.links ;
+
+
+---drop table if exists		(DATA WILL LOSS)
+drop table if exists ict_2020.links ;
+
+
+--add a new column in existing table   (DATA WILL NOT LOSS)
+ALTER TABLE ict_2020.links
+ADD COLUMN active boolean;
+
+
+
+---recheck the table
+select * from ict_2020.links l ;
+
+
+---REMOVE THE ACTIVE COLUMN
+ALTER TABLE ict_2020.links 
+DROP COLUMN active;
+
+
+
+
+---recheck the table
+select * from ict_2020.links l ;
+
+
+--To change the name of the title column to link_title,
+ALTER TABLE ict_2020.links 
+RENAME COLUMN title TO link_title;
+
+
+
+---recheck the table
+select * from ict_2020.links l ;
+
+
+
+--add a new column in existing table   (DATA WILL NOT LOSS)
+ALTER TABLE ict_2020.links 
+ADD COLUMN target VARCHAR(10);
+
+
+
+
+---recheck the table
+select * from ict_2020.links l ;
+
+
+
+---SET A DEFAULT VALUE FOR TARGER TABLE
+ALTER TABLE ict_2020.links 
+ALTER COLUMN target
+SET DEFAULT '_blank';
+
+
+
+---recheck the table
+select * from ict_2020.links l ;
+
+
+------------------------INSERTION PART--------------------------------
+
+
+INSERT INTO ict_2020.links (link_title, url)
+VALUES('PostgreSQL Tutorial','https://www.postgresqltutorial.com/');
+
+
+
+---recheck the table
+select * from ict_2020.links l ;
+
+
+INSERT INTO ict_2020.links (link_title, url,target)
+VALUES('ICT 2020','https://www.ict.com/','TEST');
+
+
+
+
+INSERT INTO ict_2020.links (link_title, url,target)
+values ('ICT 2019','https://www.ict.com/','SKY'),
+		('ICT 2018','https://www.ict.com/','RUN');
+	
+	
+
+
+---recheck the table
+select * from ict_2020.links l ;
+
+
+------------------------UPDATE PART--------------------------------
+
+
+---update all rows value of a column
+update ict_2020.links ll set target = 'Sky Blue';
+
+
+---recheck the table
+select * from ict_2020.links l ;
+
+
+
+---update specific row value of a column
+update ict_2020.links ll set target = 'Sky Red' where link_id = 3;
+
+
+---recheck the table
+select * from ict_2020.links l ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
